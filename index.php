12b962959f3894a1a24eb10b91b49dd3b85f07b5
Jamie Howard <jamie@blackairplane.com>
2015-02-10 14:08:04 -0500

    Initial commit

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+function checkEnvFile($file) {
+	$handle = file($file);
+	$envVariables = [];
+	foreach ($handle as $var):
+		if ( strpos($var, "=") ):
+			$var = explode('=', $var);
+			$envVariables[$var[0]] = $var[1];
+		else:
+			throw new Exception('Invalid environment configuration');
+		endif;
+	endforeach;
+
+	$required = ['APP_ENV', 'DB_HOST', 'DB_NAME', 'DB_PASS', 'DB_USER'];
+	foreach ($required as $var):
+		if ( ! array_key_exists($var, $envVariables) || ! $envVariables[$var]):	
+			throw new Exception('Invalid environment configuration');
+		else:
+			// Environment variable exists, so register it
+			$_ENV[$var] = trim($envVariables[$var]);
+		endif;
+	endforeach;
+}
+
+try {
+	checkEnvFile('.env');
+} catch (Exception $e) {
+	echo 'Caught exception: ', $e->getMessage(), "\n";
+}
+
+var_dump($_ENV);
+exit();
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Env-test
+####### A simple PHP script to import `.env` file and set as `$_ENV`
+This is a very rudimentary script to be improved upon later. Just documenting the idea here.
+
+The `.gitignore` file has to include `*.env` or else the environment variables are worthless
+
+### Sample .env file:
+```
+APP_ENV=local
+DB_HOST=localhost
+DB_USER=root
+DB_PASS=secret
+DB_NAME=database
+```
